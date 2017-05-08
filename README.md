31b8752bbd1e301a612b89ea35f3fa0f088c173a
stream <penolove15@gmail.com>
2017-05-08 15:24:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Docker_BDAP_wrapper
+
+#cloning 4 repository
+./git_clone.sh
---
+++ git_clone.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/penolove/Docker_Zookeeper.git
+git clone https://github.com/penolove/Docker_kafka.git
+git clone https://github.com/penolove/Docker_Spark.git
+git clone https://github.com/penolove/Docker_Hadoop.git
+git clone https://github.com/penolove/Docker_Streams.git
